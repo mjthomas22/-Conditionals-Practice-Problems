e952200fbc8542b8f9ed20897efdaef31fb62b5b
mjthomas22 <mthomas22@gmail.com>
2017-06-29 10:23:24 -0400

    commit

--- ConditionalsPracticeProblems/Program.cs
+++ ConditionalsPracticeProblems/Program.cs
@@ -17,45 +17,48 @@ namespace ConditionalsPracticeProblems
             Console.WriteLine("Please kindly enter the first number, and remember whole numbers only please.");
             int firstNumber = int.Parse(Console.ReadLine());
 
-            if (firstNumber < 1 || firstNumber > 9)
-            {
-                Console.WriteLine("Next time please follow instructions.");
-            }
-
             Console.WriteLine("Now please enter your second number.");
             int secondNumber = int.Parse(Console.ReadLine());
 
-            if (secondNumber < 1 || secondNumber > 9)
+            if (firstNumber < 1 || firstNumber > 9 || secondNumber < 1 || secondNumber > 9)
             {
-                Console.WriteLine("Next time please follow instructions.");
+                Console.WriteLine("Next time please follow instructions. You broke me!");
             }
 
-            //Player picks his number
-            Console.WriteLine("Now that you have picked your first two numbers it all comes down to this!");
-            Console.WriteLine("Please pick a special number to see if you Fizz! Buzz! or Bust!");
-            int playersNumber = int.Parse(Console.ReadLine());
+           
 
-            //assign one for fizz and buzz
+            else 
+            {
+                //Player picks his number
+                Console.WriteLine("Now that you have picked your first two numbers it all comes down to this!");
+                Console.WriteLine("Please pick a special number to see if you Fizz! Buzz! or Bust!");
+                int playersNumber = int.Parse(Console.ReadLine());
 
-            double fizz = playersNumber % firstNumber;
-            double buzz = playersNumber % secondNumber;
+                //assign one for fizz and buzz
 
-            //test if numbers are multiples for fizz and buzz
-            if (fizz == 0 && buzz == 0)
-            {
-                Console.WriteLine("FIZZBUZZ!");
-            }
-            else if (fizz == 0)
-            {
-                Console.WriteLine("FIZZ!");
-            }
-            else if (buzz == 0)
-            {
-                Console.WriteLine("BUZZ!");
-            }
-            else
-            {
-                Console.WriteLine("{0} better luck next time!", playersNumber);
+                double fizz = playersNumber % firstNumber;
+                double buzz = playersNumber % secondNumber;
+
+                //test if numbers are multiples for fizz and buzz
+                if (fizz == 0 && buzz == 0)
+                {
+                    Console.WriteLine("FIZZBUZZ!");
+                }
+                else if (fizz == 0)
+                {
+                    Console.WriteLine("FIZZ!");
+                }
+                else if (buzz == 0)
+                {
+                    Console.WriteLine("BUZZ!");
+                }
+                else
+                {
+                    Console.WriteLine("{0} better luck next time!", playersNumber);
+
+                }
+
+            
 
             }
 
